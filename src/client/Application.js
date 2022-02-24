d0c8f0051c3ce40d35bba72e5ed9053fee3373f7
AmmarNaser <92523189+AmmarNaser@users.noreply.github.com>
2022-02-25 00:33:43 +0200

    Update component and styling

--- src/client/Application.js
+++ src/client/Application.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { handleSubmit } from "./helpers/handleSubmit";
 import { checkUrl } from "./helpers/checkUrl";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  InputGroup,
+  Navbar,
+} from "react-bootstrap";
 
 export default function App() {
   const [loading, setloading] = useState(false);
@@ -32,46 +40,47 @@ export default function App() {
   };
 
   return (
-    <div className="mainPage">
-      <form onSubmit={submitHandler} className="{classes.mainForm}">
-        <div>
-          <label htmlFor="enter-url" className="{classes.mainLabel}">
-            Input your URL
-          </label>
-          <input
-            type="text"
-            placeholder="  please input URL"
-            id="enter-url"
-            onChange={(e) => setURL(e.target.value)}
-          />
+    <div>
+      <Navbar className="navbar" variant="dark">
+        <Container>
+          <Navbar.Brand className="navbar-text">
+            Evaluate News Articles
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <div className="maincontainer">
+        <div className=" input-field">
+          <Form onSubmit={submitHandler}>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="please input URL"
+                id="enter-url"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+                onChange={(e) => setURL(e.target.value)}
+                className="inputs"
+              />
+              <Button variant="outline-light" id="button-addon2" type="submit">
+                OK
+              </Button>
+            </InputGroup>
+          </Form>
         </div>
-
-        <button>Submit</button>
-      </form>
-
-      <div>
-        {loading && (
-          <div id="loadFacebookG">
-            <div id="blockG_1" className="facebook_blockG"></div>
-            <div id="blockG_2" className="facebook_blockG"></div>
-            <div id="blockG_3" className="facebook_blockG"></div>
+        <div> {loading && <div className="loader"></div>}</div>
+        <div className=" result-field">
+          <div>
+            <ul>
+              {result &&
+                Object.entries(result).map(([key, value]) => (
+                  <li key={key}>
+                    {key} : {value}
+                  </li>
+                ))}
+            </ul>
           </div>
-        )}
-      </div>
-
-      <section>
-        <h3> Results </h3>
-        <div>
-          <ul>
-            {result &&
-              Object.entries(result).map(([key, value]) => (
-                <li key={key}>
-                  {key} : {value}
-                </li>
-              ))}
-          </ul>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
